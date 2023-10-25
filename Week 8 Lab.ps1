154e1898469c8e5b247d86f61edab8014978525e
jolewiler9 <89813772+jolewiler9@users.noreply.github.com>
2023-10-24 21:12:56 -0400

    Week 8 - Intro to Powershell Lab
    Created a script to view and print Event Log entries as well as filter them for strings

---
+++ Week 8 Lab.ps1
@@ -0,0 +1,15 @@
+﻿# Storyline: Review the Security Event Log
+
+# Directory for Event Log to be stored
+$myDir = "C:\Users\champuser\Desktop\"
+
+# List all available Event Logs
+Get-EventLog -list
+
+# Create a prompt for the user to both select a log to view and filter strings from that log 
+$readLog = Read-host -Prompt "Please select a log to review from the list above"
+$searchString = Read-Host -Prompt "Please enter search string to be filtered"
+
+# Print the results for the log
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike "*$searchString*"} | export-csv -NoTypeInformation `
+-Path "$myDir\securityLogs.csv"
