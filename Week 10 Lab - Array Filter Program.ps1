1c6acb7a5995bb290cb4aff0f5cd4e4e2072d841
jolewiler9 <89813772+jolewiler9@users.noreply.github.com>
2023-11-07 20:41:09 -0500

    Week 10 - Array Filter Program
    This lab we needed to write a program that allowed users to filter installed services by 3 different values for their status. This was "Stopped", "Running", and "all". I utilized an array with those three values and used a series of if and elseif statements to run through the possibilities and prevent mistakes.

---
+++ Week 10 Lab - Array Filter Program.ps1
@@ -0,0 +1,42 @@
+﻿# Storyline: Print a list of Services and allow filtering of them by Status
+function service_check(){
+
+    cls
+
+    # List current services
+    $Services = Get-Service | Select DisplayName
+    $Services | Out-Host
+
+    # Array for services
+    $arrServices = @("all", "Stopped", "Running")
+
+    $readServices = Read-Host -Prompt "Please enter a status you would like to filter for or q to quit the program"
+
+    # Check for quit
+    if ($readServices -match "^[qQ]$"){
+
+        break
+
+    } elseif ($arrServices -notcontains $readServices){
+
+        Write-Host "Invalid choice. Filters are 'all', 'Stopped', 'Running'"
+
+        sleep 5
+
+        service_check
+
+    } elseif ($readServices -ne "all"){
+
+        Get-Service | Where-Object {$_.Status -eq $readServices} | Select-Object Status, DisplayName
+
+    } else {
+
+        Get-Service | Select-Object Status, DisplayName | Sort-Object Status
+
+    }
+
+
+}
+
+service_check
+
